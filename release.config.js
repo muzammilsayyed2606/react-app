c0b4ab9fe241475c8377cfac5b2d5d204ee6ca4d
muzammil.sayyed <muzammil.sayyed@eventussystems.com>
2022-11-14 01:09:00 +0530

    added plugins

---
+++ release.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    branches: "master",
+    repositoryUrl: "https://github.com/muzammilsayyed2606/react-app",
+    plugins: [
+          '@semantic-release/commit-analyzer',
+          '@semantic-release/release-notes-generator',
+          '@semantic-release/github'
+        ]
+}
