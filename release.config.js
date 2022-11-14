0615c68fc6c078f5507677ffa450afbabf694b05
muzammil.sayyed <muzammil.sayyed@eventussystems.com>
2022-11-14 17:58:18 +0530

    added coverage

--- release.config.js
+++ release.config.js
@@ -1,17 +1,17 @@
 module.exports = {
-    branches: "master",
-    repositoryUrl: "https://github.com/muzammilsayyed2606/react-app",
-    plugins: [
-          "@semantic-release/commit-analyzer",
-          "@semantic-release/release-notes-generator",
-          [
-            "@semantic-release/github", 
-            {
-              assets: [
-                  { path: "build.zip", label: "Build"},
-                  { path: "coverage.zip", label: "Coverage"}
-              ]
-            }
-         ]
-      ]
+  branches: "master",
+  repositoryUrl: "https://github.com/muzammilsayyed2606/react-app",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/github",
+      {
+        assets: [
+          { path: "build.zip", label: "Build" },
+          { path: "coverage.zip", label: "Coverage" }
+        ]
+      }
+    ]
+  ]
 };
